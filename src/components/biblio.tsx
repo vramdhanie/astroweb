1a4a6e9eaddb01db0e4088239d575cb9b1aead92
Vincent Ramdhnaie <vramdhanie@gmail.com>
2023-08-14 09:36:55 -0700

    Add dragons of eden to reading list

--- src/components/biblio.tsx
+++ src/components/biblio.tsx
@@ -12,15 +12,15 @@ const Biblio = ({
   cover: string
 }) => {
   return (
-    <div className="p-4  mx-2 flex">
-      <div className="w-24 mr-2">
-        <img src={"/images/books/" + cover} alt="book cover" />
-      </div>
-      <div className="text-right mt-2">
+    <div className="p-4  mx-2 flex justify-between w-full">
+      <div className="text-left mt-2">
         <div className="font-semibold">{title}</div>
         <div className="font-normal">{author}</div>
         <div className="font-normal text-gray-500">{year}</div>
         <div>{children}</div>
+      </div>
+      <div className="w-24 mr-2 shrink-0">
+        <img src={"/images/books/" + cover} alt="book cover" />
       </div>
     </div>
   )
