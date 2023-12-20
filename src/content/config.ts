201b46a4e8886d29ee6761cf1dd56b0e39f9353a
Vincent Ramdhanie <vramdhanie@gmail.com>
2023-12-19 18:56:13 -0800

    Add a books section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,26 @@
-import { defineCollection } from "astro:content"
-import { z } from "zod"
+import { defineCollection } from 'astro:content'
+import { z } from 'zod'
 
 const articles = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    _slug: z.string(),
-    image: z.string(),
-    author: z.string(),
-    abstract: z.string(),
-  }),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        _slug: z.string(),
+        image: z.string(),
+        author: z.string(),
+        abstract: z.string(),
+    }),
 })
-export const collections = { articles }
+
+const books = defineCollection({
+    type: 'content',
+    schema: z.object({
+        blurb: z.string(),
+        year: z.string(),
+        _slug: z.string(),
+        image: z.string(),
+    }),
+})
+
+export const collections = { articles, books }
